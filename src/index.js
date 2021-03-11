1f18e4ee97cb03437ed11c31912b3494c0a1d290
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-03-11 23:05:50 +0300

    docs: update index.html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+if (array !== undefined && array.length > 0) {
+    let min_value = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (min_value > array[i]) {
+            min_value = array[i];
+        }
+        }
+return min_value;
+        }
+    else {
+        return 0;
+    }
+    }
 
 exports.max = function max (array) {
-  return 0;
+    if (array !== undefined && array.length > 0) {
+    let max_value = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (max_value < array[i]) {
+            max_value = array[i];
+        }
+        }
+return max_value;
+        }
+    else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array !== undefined && array.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let avg_array = sum / array.length
+        return avg_array
+    } else return 0;
 }
